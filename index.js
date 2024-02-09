629720f544fca512996c9e25348e0facc2c3e5e5
BabaYaaga10 <159010854+BabaYaaga10@users.noreply.github.com>
2024-02-09 15:42:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+// const sum2= (num1,num2) => {
+//     return num1+num2const a=[1,2,3,4,5,6]
+// a.push(100)
+// console.log(a)
+// a.pop()
+// console.log(a)
+// let obj1= {
+//     username:"sajlin",
+//     age:20
+// }
+// obj1.age=10
+// obj1.profession="student"
+// console.log(obj1.profession)
+// for(let i=0;i<5;i++){
+//     console.log(i);
+// }
+
+// function sum(num1,num2){
+//     return
+// }
+
+// function multiple(num1,num2){ 
+//     return num1*num2
+// }
+
+// 
+// 
+const arr=[1,2,3,4,5]
+const result =arr.filter((item) =>{ 
+    return item%2==0
+})
+console.log(result)
+const obj2 = {
+    name:"sajlin"
+}
+let obj3 = {...obj2}
+obj3.name="sajlin" 
+console.log(obj3);
